1aa86cd973162c3207ea7c222c6696b20994ffce
VanshVg <vansh.vg18@gmail.com>
2025-01-06 23:43:35 +0530

    added: toggle sidebar on click of sidebar icon

--- src/modules/dashboard/components/Sidebar.tsx
+++ src/modules/dashboard/components/Sidebar.tsx
@@ -3,15 +3,23 @@ import { MainLogo } from "@/common/types/constants";
 import useSidebar from "@/hooks/useSidebar";
 import SidebarElement from "./SidebarElement";
 import { Fragment } from "react/jsx-runtime";
+import { useDispatch, useSelector } from "react-redux";
+import { getSidebar, toggleSidebar } from "@/redux/slices/sidebarSlice";
 
 const Sidebar = () => {
   const { sidebarElements } = useSidebar();
+  const dispatch = useDispatch();
 
   return (
     <div className="h-[100vh] w-[291px] bg-slate-800 py-5 flex flex-col">
       <div className="flex justify-between px-5 gap-3 mb-[80px]">
         <img src={MainLogo} className="h-[70px] w-[70px] cursor-pointer" />
-        <div className="mt-6 hover: cursor-pointer">
+        <div
+          className="mt-6 hover: cursor-pointer"
+          onClick={() => {
+            dispatch(toggleSidebar());
+          }}
+        >
           <SidebarIcon />
         </div>
       </div>
